ab84b86d57fa38c9285374e66feddd9b9b200719
bigOrange123456789 <bigOrange123456789>
2023-04-11 00:15:37 +0800

    给出了P2P控制服务器

--- server_p2p_controller.js
+++ server_p2p_controller.js
@@ -1,17 +1,17 @@
-var nodeStatic = require('node-static')
-var http = require('http')
-var fileServer = new(nodeStatic.Server)()
-var app = http.createServer(function(req, res) {
-  fileServer.serve(req, res)
-}).listen(8011)
-var io = require('socket.io').listen(app)
-io.sockets.on('connection', socket=> {
-  socket.on('send', message=> socket.broadcast.emit('receive', message))
-})
-function test(){
-  const connectedClients = io.sockets.sockets
-  for (const id in connectedClients) {
-    const socket=connectedClients[id]
-    console.log(id,socket.number)
-  }
+const ioUrlList = [
+  "114.80.207.60",
+  "1.13.198.213"
+]
+let ioList=[]
+for(let i=0;i<ioUrlList.length;i++){
+  let ioList0=[]
+  for(let j=0;j<ioUrlList.length;j++)
+    if(j!==i)ioList0.push(ioUrlList[j])
+  const io = require('socket.io-client')("http://"+ioUrlList[i]+":8011")
+  io.ioUrlList=ioList0
+  ioList.push(io)
 }
+ioList.forEach(io => {
+  io.emit('updateIoList', {"ioUrlList":io.ioUrlList})
+  console.log({"ioUrlList":io.ioUrlList})
+})
--- server_p2p_edge.js
+++ server_p2p_edge.js
@@ -1,3 +1,4 @@
+const ioList=[]
 var nodeStatic = require('node-static')
 var http = require('http')
 var fileServer = new(nodeStatic.Server)()
@@ -6,37 +7,32 @@ var app = http.createServer(function(req, res) {
 }).listen(8011)
 var io = require('socket.io').listen(app)
 io.sockets.on('connection', socket=> {
+  socket.on('updateIoList', data => {
+    updateIoList(data.ioUrlList)
+  })
+  socket.on('edge2edge', data => {
+    send2Page(data)
+  })
   socket.on('send', message=> {
     socket.broadcast.emit('receive', message)
     send2Edge(message)
   })
 })
+function updateIoList(ioUrlList ){
+  ioUrlList.forEach(ioUrl => {
+    const io = require('socket.io-client')("http://"+ioUrl+":8011")
+    ioList.push(io)
+  })
+}
+function send2Edge(data){
+  ioList.forEach(io => {
+    io.emit('edge2edge', data);
+  })
+}
 function send2Page(data){
   const connectedClients = io.sockets.sockets
   for (const id in connectedClients) {
     const socket=connectedClients[id]
     socket.emit('receive',data)
   }
-}
-
-const ioUrlList = [
-  "114.80.207.60",
-  "1.13.198.213"
-]
-const ioList=[]
-ioUrlList.forEach(ioUrl => {
-  const io = require('socket.io-client')("http://"+ioUrl+":8032")
-  ioList.push(io)
-})
-const io1 = require('socket.io')()
-io1.listen(8032);
-io1.on('connection', socket => {
-  socket.on('edge2edge', data => {
-    send2Page(data)
-  })
-})
-function send2Edge(data){
-  ioList.forEach(io => {
-    io.emit('edge2edge', data);
-  })
 }
