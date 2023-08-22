c9922c8cc260b8800645721bb21886862724ec17
bigOrange123456789 <bigOrange123456789>
2023-08-22 19:53:36 +0800

    save

--- src/LoadingProgressive_old/postprocessing/src2/temporal-reproject/pass/VelocityPass.js
+++
@@ -1,7 +0,0 @@
-import { VelocityDepthNormalPass } from "./VelocityDepthNormalPass"
-
-export class VelocityPass extends VelocityDepthNormalPass {
-	constructor(scene, camera) {
-		super(scene, camera, false)
-	}
-}
