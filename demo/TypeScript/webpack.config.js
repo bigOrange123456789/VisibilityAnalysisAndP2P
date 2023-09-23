547c68a4fd95ff5246da992aeacc31a532186e76
bigOrange123456789 <bigOrange123456789>
2023-09-24 04:06:18 +0800

    实现了webpack打包ts代码

--- demo/TypeScript/webpack.config.js
+++ demo/TypeScript/webpack.config.js
@@ -12,7 +12,19 @@ module.exports = {
     output: {
         path: __dirname ,  
         filename: 'index.js'  
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.tsx?$/, // 匹配 .ts 或 .tsx 文件
+            use: 'ts-loader', // 使用 ts-loader 处理 TypeScript 文件
+            exclude: /node_modules/, // 排除 node_modules 目录
+          },
+        ],
+      },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'], // 解析这些文件的扩展名
+    },
 };
 //ES6 模块
 //....
