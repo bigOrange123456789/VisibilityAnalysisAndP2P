99ac59450540ee862ba16a232b545397acade745
bigOrange123456789 <bigOrange123456789>
2024-01-10 14:24:27 +0800

    成功测试了'通过vscode使用cmake'

---
+++ tool_crowd/cpp_test/hello.cpp
@@ -0,0 +1,6 @@
+#include"iostream"
+using namespace std;
+int main(void){
+    cout<<"say hello world gakki"<<endl;
+    return 0;
+}
---
+++ tool_crowd/cpp_test/hello.h
@@ -0,0 +1,6 @@
+#ifndef HELLO_H
+#define HELLO_H
+namespace hello{
+    void say_hello();
+}
+#endif
