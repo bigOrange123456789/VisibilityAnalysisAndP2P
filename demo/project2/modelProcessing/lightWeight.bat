ca6206a6bc0f8f899ae4d5dcc73e653017a149b8
bigOrange123456789 <bigOrange123456789>
2024-04-18 01:44:43 +0800

    新增project2

---
+++ demo/project2/modelProcessing/lightWeight.bat
@@ -0,0 +1,11 @@
+set sceneName=bigCity
+set insertPath=project1\modelProcessing
+set root_dir=%cd%\..\..\..\
+set inpath=%root_dir%dist\assets\models\%sceneName%.zip
+set outpath=%root_dir%dist\assets\models
+cd ..\..\..\
+node %cd%\demo\%insertPath%\updatePackage.js %insertPath% %sceneName%
+"SLMConvertor/SLMConvertor.exe" -t "%cd%\demo\%insertPath%\task.json" -dir "SLMConvertor/tools/wdir" -format "Bos3"
+node %cd%\demo\%insertPath%\move.js %root_dir%\assets\models\%sceneName%_output/full/%sceneName%_output.zip %inpath%
+npx electron . %inpath% %outpath%
+pause
